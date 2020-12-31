48196411a09abd5057892e620d61e6f0a827aed7
Joakim Romland <romland@gmail.com>
2020-12-31 15:29:18 +0100

    One more 'fix'

--- client/src/lib/convex-hull.js
+++ client/src/lib/convex-hull.js
@@ -24,7 +24,7 @@ function outline(context, rs, points)
 	}
 
 	context.beginPath();
-	context.moveTo(outline[0].x * rs, outline[0].y * rs);
+	context.moveTo((outline[0].x+1) * rs, outline[0].y * rs);		// XXX: 1 off fix here too (see below)
 	for (var i = 0; i < outline.length; i++) {
 		outline[i].x += 1;		// XXX: At some point we have a one-off error in visualization. We fix it here :(
 		context.lineTo(outline[i].x * rs, outline[i].y * rs);
